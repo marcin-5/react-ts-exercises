05837ddd0179da9b5fa681d8c3c913b27cb19b86
Marcin Bojara <marcin_5@mail.ru>
2023-12-11 17:28:57 +0100

    handle buttons for add and delete goal

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,39 @@
+import { useState } from 'react';
+import goalsImg from './assets/goals.jpg';
+import CourseGoalList from './components/CourseGoalList.tsx';
+import Header from './components/Header.tsx';
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!',
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  }
+
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
+    </main>
+  );
 }
