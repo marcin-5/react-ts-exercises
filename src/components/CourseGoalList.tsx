6b2d687a0e08d43fc3cd8c79409239290672984d
Marcin Bojara <marcin_5@mail.ru>
2023-12-11 19:55:12 +0100

    use component with Discriminated Unions

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -18,7 +18,7 @@ export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListPr
 
   if (goals.length >= 4) {
     warningBox = (
-      <InfoBox mode="warning">
+      <InfoBox mode="warning" severity="medium">
         You're collecting a lot of goals. Don't put too much on your plate!
       </InfoBox>
     );
